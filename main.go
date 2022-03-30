253a99aea1358c2bd5fea64049061684eb31d527
liwen <valar01@163.com>
2022-03-30 17:51:01 +0800

    模板渲染优化

--- main.go
+++ main.go
@@ -11,6 +11,16 @@ import (
 	"time"
 )
 
+type S2 struct {
+	Title string
+	Content []model.Item
+}
+
+type S1 struct {
+	Flag int
+	Style int
+	Data []S2
+}
 
 func main()  {
 	Run()
@@ -53,14 +63,104 @@ func RunServer() {
 		tmp := make(map[string]interface{})
 		tmp["Flag"] = 0
 		tmp["Data"] = model.M["Weibo"]
-		c.HTML(200, "content.html",tmp)
+		c.HTML(200, "onelist.html",tmp)
 	})
 
 	r.GET("/hot", func(c *gin.Context) {
 		tmp := make(map[string]interface{})
 		tmp["Flag"] = 0
 		tmp["Data"] = model.M["Weibo"]
-		c.HTML(200, "content.html",tmp)
+		c.HTML(200, "onelist.html",tmp)
+	})
+
+	r.GET("/weibo", func(c *gin.Context) {
+		tmp := make(map[string]interface{})
+		tmp["Flag"] = 0
+		tmp["Data"] = model.M["Weibo"]
+		c.HTML(200, "onelist.html",tmp)
+	})
+
+	r.GET("/baidu", func(c *gin.Context) {
+		tmp := make(map[string]interface{})
+		tmp["Flag"] = 0
+		tmp["Data"] = model.M["Baidu"]
+		c.HTML(200, "onelist.html",tmp)
+	})
+
+	r.GET("/csdn", func(c *gin.Context) {
+		tmp := make(map[string]interface{})
+		tmp["Flag"] = 0
+		tmp["Data"] = model.M["CSDN"]
+		c.HTML(200, "onelist.html",tmp)
+	})
+
+	r.GET("/zhihu", func(c *gin.Context) {
+		tmp := make(map[string]interface{})
+		tmp["Flag"] = 0
+		tmp["Data"] = model.M["Zhihu"]
+		c.HTML(200, "onelist.html",tmp)
+	})
+
+	r.GET("/acfun", func(c *gin.Context) {
+		tmp := make(map[string]interface{})
+		tmp["Flag"] = 0
+		tmp["Data"] = model.M["Acfun"]
+		c.HTML(200, "onelist.html",tmp)
+	})
+
+	r.GET("/blbl", func(c *gin.Context) {
+		tmp := make(map[string]interface{})
+		tmp["Flag"] = 0
+		tmp["Data"] = model.M["Bilibili"]
+		c.HTML(200, "onelist.html", tmp)
+	})
+
+	r.GET("/github", func(c *gin.Context) {
+		var R S1
+		R.Flag = 1
+		R.Style = 20
+		R.Data = append(R.Data,
+			S2{Title: "Daily", Content: model.M["Github3"]},
+			S2{Title: "Weekly", Content: model.M["Github4"]},
+			S2{Title: "Monthly", Content: model.M["Github5"]},
+		)
+		c.HTML(200, "threelist.html",R)
+	})
+
+	r.GET("/githubGo", func(c *gin.Context) {
+		var R S1
+		R.Flag = 1
+		R.Style = 20
+		R.Data = append(R.Data,
+			S2{Title: "Daily(Go)", Content: model.M["Github0"]},
+			S2{Title: "Weekly(Go)", Content: model.M["Github1"]},
+			S2{Title: "Monthly(Go)", Content: model.M["Github2"]},
+		)
+		c.HTML(200, "threelist.html",R)
+	})
+
+	r.GET("/juejin", func(c *gin.Context) {
+		var R S1
+		R.Flag = 1
+		R.Style = 10
+		R.Data = append(R.Data,
+			S2{Title: "Triduum", Content: model.M["Juejin3"]},
+			S2{Title: "Weekly", Content: model.M["Juejin7"]},
+			S2{Title: "Monthly", Content: model.M["Juejin30"]},
+		)
+		c.HTML(200, "threelist.html",R)
+	})
+
+	r.GET("/kr36", func(c *gin.Context) {
+		var R S1
+		R.Flag = 1
+		R.Style = 10
+		R.Data = append(R.Data,
+			S2{Title: "人气榜", Content: model.M["Kr360"]},
+			S2{Title: "综合榜", Content: model.M["Kr361"]},
+			S2{Title: "收藏榜", Content: model.M["Kr362"]},
+		)
+		c.HTML(200, "threelist.html",R)
 	})
 
 	r.GET("/tool", func(c *gin.Context) {
@@ -72,76 +172,6 @@ func RunServer() {
 			return
 		}
 		c.HTML(200, "tool.html",v)
-	})
-
-	r.GET("/csdn", func(c *gin.Context) {
-		tmp := make(map[string]interface{})
-		tmp["Flag"] = 0
-		tmp["Data"] = model.M["CSDN"]
-		c.HTML(200, "content.html",tmp)
-	})
-
-	r.GET("/zhihu", func(c *gin.Context) {
-		tmp := make(map[string]interface{})
-		tmp["Flag"] = 0
-		tmp["Data"] = model.M["Zhihu"]
-		c.HTML(200, "content.html",tmp)
-	})
-
-	r.GET("/weibo", func(c *gin.Context) {
-		tmp := make(map[string]interface{})
-		tmp["Flag"] = 0
-		tmp["Data"] = model.M["Weibo"]
-		c.HTML(200, "content.html",tmp)
-	})
-
-	r.GET("/baidu", func(c *gin.Context) {
-		tmp := make(map[string]interface{})
-		tmp["Flag"] = 0
-		tmp["Data"] = model.M["Baidu"]
-		c.HTML(200, "content.html",tmp)
-	})
-
-	r.GET("/acfun", func(c *gin.Context) {
-		tmp := make(map[string]interface{})
-		tmp["Flag"] = 0
-		tmp["Data"] = model.M["Acfun"]
-		c.HTML(200, "content.html",tmp)
-	})
-
-	r.GET("/blbl", func(c *gin.Context) {
-		tmp := make(map[string]interface{})
-		tmp["Flag"] = 0
-		tmp["Data"] = model.M["Bilibili"]
-		c.HTML(200, "content.html", tmp)
-	})
-
-	r.GET("/github", func(c *gin.Context) {
-		tmp := make(map[string]interface{})
-		tmp["Flag"] = 1
-		tmp["Data"] = model.M
-		c.HTML(200, "github.html",tmp)
-	})
-
-	r.GET("/githubGo", func(c *gin.Context) {
-		tmp := make(map[string]interface{})
-		tmp["Flag"] = 2
-		tmp["Data"] = model.M
-		c.HTML(200, "githubGo.html", tmp)
-	})
-
-	r.GET("/juejin", func(c *gin.Context) {
-		tmp := make(map[string]interface{})
-		tmp["Flag"] = 3
-		tmp["Data"] = model.M
-		c.HTML(200, "juejin.html",tmp)
-	})
-
-	r.GET("/kr36", func(c *gin.Context) {
-		tmp := make(map[string]interface{})
-		tmp["Flag"] = 4
-		tmp["Data"] = model.M
-		c.HTML(200, "kr36.html",tmp)
 	})
 
 	r.Run("127.0.0.1:8080")
